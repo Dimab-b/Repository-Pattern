e31137af2313b8efacb475a4dafd2820034ed9c1
Dimab-b <dimoncikbatig8@gmail.com>
2026-05-04 23:27:34 +0300

    Update AppDbContext.cs

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -1,6 +1,18 @@
-﻿namespace SecondSiteArchitecture.Data
+using Microsoft.EntityFrameworkCore;
+using SecondSiteArchitecture.Models;
+
+namespace SecondSiteArchitecture.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public DbSet<Order> orders { get; set; } = null!;
+        public DbSet<Category> categories { get; set; } = null!;
+        public DbSet<Product> products { get; set; } = null!;
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        
+
     }
 }
