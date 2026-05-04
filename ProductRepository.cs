48e7513d5a9493d1cb4b3e4b4c9f3380f2ddcf08
Dimab-b <dimoncikbatig8@gmail.com>
2026-05-04 23:29:55 +0300

    Update ProductRepository.cs

--- ProductRepository.cs
+++ ProductRepository.cs
@@ -1,6 +1,22 @@
-﻿namespace SecondSiteArchitecture.Data.Reposetories
+using Microsoft.EntityFrameworkCore;
+using SecondSiteArchitecture.Models;
+using SecondSiteArchitecture.Data;
+
+namespace SecondSiteArchitecture.Data.Reposetories
 {
-    public class ProductRepository
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
     {
+        private readonly AppDbContext _context;
+
+        public ProductRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+
+        public IQueryable<Product> SearchProduct(string name)
+        {
+            return _context.products.AsQueryable().Where(p => p.Name.Contains(name));
+        }
     }
 }
