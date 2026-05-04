059748e1f995c0acffdf53e9a28d8552f351be5d
Dimab-b <dimoncikbatig8@gmail.com>
2026-05-04 23:29:34 +0300

    Update OrderRepository.cs

--- OrderRepository.cs
+++ OrderRepository.cs
@@ -1,6 +1,19 @@
-﻿namespace SecondSiteArchitecture.Data.Reposetories
+using SecondSiteArchitecture.Models;
+
+namespace SecondSiteArchitecture.Data.Reposetories
 {
-    public class OrderRepository
+    public class OrderRepository : GenericRepository<Order>, IOrderRepository
     {
+        private readonly AppDbContext _context;
+
+        public OrderRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+        public async Task<Order> AddOrder(Order order)
+        {
+            var result = await _context.orders.AddAsync(order);
+            return result.Entity;
+        }
     }
 }
