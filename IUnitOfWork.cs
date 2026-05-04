ff249cf8fb5d103c7a349bc1b78807b093e466f0
Dimab-b <dimoncikbatig8@gmail.com>
2026-05-04 23:29:15 +0300

    Update IUnitOfWork.cs

--- IUnitOfWork.cs
+++ IUnitOfWork.cs
@@ -1,6 +1,11 @@
-﻿namespace SecondSiteArchitecture.Data
+using SecondSiteArchitecture.Data.Reposetories;
+
+namespace SecondSiteArchitecture.Data
 {
-    public interface IUnitOfWork
+    public interface IUnitOfWork : IDisposable
     {
+        IProductRepository products { get; }
+        IOrderRepository orders { get; }
+        Task CompleteAsync();
     }
 }
