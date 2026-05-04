fd776d2ba05f2d448e6ca60d8007194a7144ca51
Dimab-b <dimoncikbatig8@gmail.com>
2026-05-04 23:30:16 +0300

    Update UnitOfWork.cs

--- UnitOfWork.cs
+++ UnitOfWork.cs
@@ -1,6 +1,28 @@
-﻿namespace SecondSiteArchitecture.Data
+using SecondSiteArchitecture.Data.Reposetories;
+
+namespace SecondSiteArchitecture.Data
 {
-    public class UnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
+        private readonly AppDbContext _context;
+        public IProductRepository products { get; }
+        public IOrderRepository orders { get; }
+
+        public UnitOfWork(AppDbContext context, IProductRepository productRepository, IOrderRepository orderRepository)
+        {
+            _context = context;
+            products = productRepository;
+            orders = orderRepository;
+        }
+
+        public async Task CompleteAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
     }
 }
